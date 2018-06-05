f4ae266f2ea40b3898634a7b0e714418db24aad5
Tiago Soczek <tiagosoczek@gmail.com>
2018-06-05 11:29:22 -0300

    Fix namespace

--- src/samples/TMS/Hotels/Commands/SearchHotelsCommand.cs
+++ src/samples/TMS/Hotels/Commands/SearchHotelsCommand.cs
@@ -2,7 +2,7 @@
 using MediatR;
 using Zek.Shared;
 
-namespace TMS.Api.Flights.Commands
+namespace TMS.Hotels.Commands
 {
     // https://developers.amadeus.com/self-service/category/202/api-doc/11/api-docs-and-example/10009
     public class SearchHotelsCommand : IRequest<Result>
